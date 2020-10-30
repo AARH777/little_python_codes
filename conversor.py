cad441c99af12b0029d49bce7988e24463c24863
Alex Reyes <AlexReyesRH96@outlook.com>
2020-10-30 15:49:49 -0600

    Primera versión del conversor

---
+++ conversor.py
@@ -0,0 +1,8 @@
+mexican_pesos = float(input('How many money do you have? '))
+
+dollar_value = 21.19
+dollars = mexican_pesos / dollar_value
+dollars = round(dollars, 2)
+dollars = str(dollars)
+
+print('Do you have $' + dollars + ' dollars')
