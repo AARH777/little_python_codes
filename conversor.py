44d65de9db30464c8b970f23040304f02b79ec1c
Alex Reyes <73624939+AARH777@users.noreply.github.com>
2020-11-05 10:04:08 -0600

    Código con espaciado más adecuado

--- conversor.py
+++ conversor.py
@@ -1,7 +1,9 @@
 mexican_pesos = float(input('How many money do you have? '))
 
 dollar_value = 21.21
+
 dollars = mexican_pesos / dollar_value
+
 dollars = round(dollars, 2)
 dollars = str(dollars)
 
